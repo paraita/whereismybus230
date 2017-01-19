521cc61799f977b83064758e3a59d8a400fe7de4
Paraita Wohler <milkyboi@gmail.com>
2017-01-19 16:24:07 +0100

    GAEify the project and add the return model for the swagger interface

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 
 from flask import Flask
-from flask_restplus import Resource, Api
+from flask_restplus import Resource
+from flask_restplus import Api
+from flask_restplus import fields
 import sophiabus230
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='Whereismybus230',
           description='Dude, where is my bus 230 ?!')
 
+BusPassage = api.model('BusPassage', {
+    'bus_time': fields.String,
+    'dest': fields.String,
+    'is_real_time': fields.Boolean
+})
+
 
 @api.route('/bus230')
 class HelloWorld(Resource):
 
+    @api.response(200, 'Success', [BusPassage])
     def get(self):
         tt = sophiabus230.get_next_buses()
         return [
