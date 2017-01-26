ec4a1440a551857587f851e1dfe5b8e78cd8a6f2
Paraita Wohler <milkyboi@gmail.com>
2017-01-26 21:14:28 +0100

    Add the stop-id parameter

--- whereismybus230/starter.py
+++ whereismybus230/starter.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_restplus import Resource
 from flask_restplus import Api
 from flask_restplus import fields
+from flask_restplus import reqparse
 import sophiabus230
 
 app = Flask(__name__)
@@ -11,18 +12,26 @@ api = Api(app, version='1.0', title='Whereismybus230',
           description='Dude, where is my bus 230 ?!')
 
 BusPassage = api.model('BusPassage', {
-    'bus_time': fields.String,
-    'dest': fields.String,
-    'is_real_time': fields.Boolean
+    'bus_time': fields.String(description='Time of passage in ISO8601 format',
+                              required=True),
+    'dest': fields.String(description='Terminus',
+                          required=True),
+    'is_real_time': fields.Boolean(description='Whether the time is an estimation or real time',
+                                   required=True)
 })
 
 
 @api.route('/bus230')
 class HelloWorld(Resource):
 
+    get_parser = reqparse.RequestParser()
+    get_parser.add_argument('stop_id', required=True, type=int, help='Stop ID of the bus stop')
+
+    @api.expect(get_parser)
     @api.response(200, 'Success', [BusPassage])
     def get(self):
-        tt = sophiabus230.get_next_buses()
+        args = self.get_parser.parse_args()
+        tt = sophiabus230.get_next_buses(stop_id=args['stop_id'])
         return [
             {
                 'bus_time': bus['bus_time'].isoformat(),
